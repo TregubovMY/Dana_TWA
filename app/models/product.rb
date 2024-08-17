b4a17d7b2c9e56e7655d741009346579d4e242ce
Tregubov Maksim <tregubov-m@inbox.ru>
2024-08-17 15:14:12 +0300

    Add simple css for store

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ApplicationRecord
 
   belongs_to :category
   has_many :orders, dependent: :destroy
-  has_one_attached :image, dependent: :destroy
+  has_one_attached :image, dependent: :destroy do |photo|
+    photo.variant :preview, resize_to_fill: [200, 150], preprocessed: true
+  end
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
